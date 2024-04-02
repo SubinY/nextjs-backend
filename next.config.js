182a13aa45871d2671231b92fc905f91d70b7129
huxiubin <huxiubin@itouchtv.cn>
2024-04-02 17:49:51 +0800

    config

--- next.config.js
+++ next.config.js
@@ -5,12 +5,42 @@ module.exports = {
   async headers() {
     return [
       {
-        // Apply these headers to all routes in your application.
-        source: "/:path*",
+        source: '/api/:path*',
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-        ],
-      },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*' // 允许任何源
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,POST,PUT,DELETE' // 允许的 HTTP 方法
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Content-Type, Accept, Authorization' // 允许的请求头
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: true // 是否允许发送 cookies
+          }
+        ]
+      }
     ];
   },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: '/api/:path*',
+  //       has: [
+  //         {
+  //           type: 'header',
+  //           key: 'cookie',
+  //           value: false
+  //         }
+  //       ]
+  //     }
+  //   ];
+  // }
 };
